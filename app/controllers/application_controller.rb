de9da1cd30ff6d271a5e885f8853b479121ef44d
Nathan Bolser <nbolser@gmail.com>
2018-01-04 00:25:49 -0700

    Update application configs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
   include ActionController::MimeResponds
-  protect_from_forgery with: :null_session
 
   private
     def token
